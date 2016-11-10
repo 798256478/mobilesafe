77c0622acaff6dfb8020419953352e1cabf46898
zhaowenbin <798256478@qq.com>
2016-11-10 17:31:46 +0800

    悬浮框样式改变

--- src/com/zahowenbin/mobilesafe/activity/RocketBgActivity.java
+++ src/com/zahowenbin/mobilesafe/activity/RocketBgActivity.java
@@ -1,6 +1,8 @@
 package com.zahowenbin.mobilesafe.activity;
 
 import com.zahowenbin.mobilesafe.R;
+import com.zahowenbin.mobilesafe.utils.ConstantView;
+import com.zahowenbin.mobilesafe.utils.SpUtil;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -8,24 +10,32 @@ import android.os.Handler;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 
 public class RocketBgActivity extends Activity {
+	private ImageView iv_rocket_bg_top;
+	private AlphaAnimation alphaAnimation;
+	private ImageView iv_rocket_bg_bottom;
 	private Handler mHandler =  new Handler(){
 		public void handleMessage(android.os.Message msg) {
 			finish();
 		};
 	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_rocket_bg);
-		ImageView iv_rocket_bg_top = (ImageView) findViewById(R.id.iv_rocket_bg_top);
-		ImageView iv_rocket_bg_bottom = (ImageView) findViewById(R.id.iv_rocket_bg_bottom);
-		AlphaAnimation alphaAnimation = new AlphaAnimation(0, 1);
+		iv_rocket_bg_top = (ImageView) findViewById(R.id.iv_rocket_bg_top);
+		iv_rocket_bg_bottom = (ImageView) findViewById(R.id.iv_rocket_bg_bottom);
+		alphaAnimation = new AlphaAnimation(0, 1);
 		alphaAnimation.setDuration(500);
 		iv_rocket_bg_bottom.startAnimation(alphaAnimation);
+		alphaAnimation.setDuration(1000);
 		iv_rocket_bg_top.startAnimation(alphaAnimation);
+
 		
 		mHandler.sendEmptyMessageDelayed(0, 1000);
 	}
