364e75b77a51bf0a8fb94e8482fcdb2e85159f7e
zhaowenbin <798256478@qq.com>
2016-11-10 10:10:55 +0800

    短信备份

--- src/com/zahowenbin/mobilesafe/activity/AtoolsActivity.java
+++ src/com/zahowenbin/mobilesafe/activity/AtoolsActivity.java
@@ -1,11 +1,18 @@
 package com.zahowenbin.mobilesafe.activity;
 
 
+import java.io.File;
+
 import com.zahowenbin.mobilesafe.R;
+import com.zahowenbin.mobilesafe.engine.SmsBackUp;
+import com.zahowenbin.mobilesafe.engine.SmsBackUp.CallBack;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Path;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
@@ -15,10 +22,47 @@ public class AtoolsActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_atools);
-		initUI();
+		initQueryAddress();
+		initBackupSms();
 	}
 
-	private void initUI() {
+	private void initBackupSms() {
+		TextView tv_sms_backup = (TextView) findViewById(R.id.tv_sms_backup);
+		tv_sms_backup.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				showBackUpDialog();
+				
+			}
+		});
+	}
+
+	protected void showBackUpDialog() {
+		final ProgressDialog progressDialog = new ProgressDialog(this);
+		progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		progressDialog.setTitle("正在备份");
+		progressDialog.show();
+		new Thread(){
+			public void run() {
+				String path = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "smsBack.xml";
+				SmsBackUp.backUp(getApplicationContext(), path, new CallBack() {
+					
+					@Override
+					public void setProgress(int index) {
+						progressDialog.setProgress(index);
+					}
+					
+					@Override
+					public void setMax(int max) {
+						progressDialog.setMax(max);
+					}
+				});
+			};
+		}.start();
+	}
+
+	private void initQueryAddress() {
 		TextView tv_query_phone = (TextView) findViewById(R.id.tv_query_phone);
 		tv_query_phone.setOnClickListener(new OnClickListener() {
 			
---
+++ src/com/zahowenbin/mobilesafe/engine/SmsBackUp.java
@@ -0,0 +1,85 @@
+package com.zahowenbin.mobilesafe.engine;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.xmlpull.v1.XmlSerializer;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Xml;
+
+public class SmsBackUp {
+	private static int index = 0;
+	public static void backUp(Context context, String path, CallBack callBack){
+		Cursor cursor = null;
+		FileOutputStream fos = null;
+		try {
+			cursor  = context.getContentResolver().query(Uri.parse("content://sms/"), new String[]{"address", "date", "type", "body"}, null, null, null);
+			File file = new File(path);
+			fos = new FileOutputStream(file);
+			int count = cursor.getCount();
+			if(callBack != null){
+				callBack.setMax(count);
+			}
+			XmlSerializer xmlSerializer = Xml.newSerializer();
+			xmlSerializer.setOutput(fos, "utf-8");
+			xmlSerializer.startDocument("utf-8", true);
+			xmlSerializer.startTag(null, "smss");
+			while (cursor .moveToNext()) {
+				
+				xmlSerializer.startTag(null, "sms");
+				
+				xmlSerializer.startTag(null, "address");
+				xmlSerializer.text(cursor .getString(0));
+				xmlSerializer.endTag(null, "address");
+				
+				xmlSerializer.startTag(null, "date");
+				xmlSerializer.text(cursor .getString(1));
+				xmlSerializer.endTag(null, "date");
+				
+				xmlSerializer.startTag(null, "type");
+				xmlSerializer.text(cursor .getString(2));
+				xmlSerializer.endTag(null, "type");
+				
+				xmlSerializer.startTag(null, "body");
+				xmlSerializer.text(cursor .getString(3));
+				xmlSerializer.endTag(null, "body");
+				
+				xmlSerializer.endTag(null, "sms");
+				
+				index++;
+				if(callBack != null){
+					callBack.setProgress(index);
+				}
+				Thread.sleep(500);
+			}
+			xmlSerializer.endTag(null, "smss");
+			xmlSerializer.endDocument();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			if(fos != null && cursor != null){
+				cursor.close();
+				try {
+					fos.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	/*
+	 * 回调（观察者模式）
+	 */
+	public interface CallBack{
+		public void setMax(int max);
+		public void setProgress(int index);
+	}
+}
+
